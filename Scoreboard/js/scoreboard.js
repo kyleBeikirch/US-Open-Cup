95980939c6e2dd1e728ffae631778f75f53d1a2d
Kyle Beikirch <kyle.beikirch@gmail.com>
2011-09-22 21:59:29 -0700

    Pull data from xml working

---
+++ Scoreboard/js/scoreboard.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "http://thecup.us/brackets/2011/us_open/data/2011bracket.xml",
+        dataType: "xml",
+        success: xmlParser
+    });
+});
+
+function xmlParser(xml) {
+
+	//console.log($(xml));
+    $(xml).find("game").each(function() {
+		
+		console.log($(this).find("stadium"));
+        //$(".main").append('<div class="book"><div class="title">' + $(this).find("Title").text() + '</div><div class="description">' + $(this).find("Description").text() + '</div><div class="date">Published ' + $(this).find("Date").text() + '</div></div>');
+        //$(".book").fadeIn(1000);
+
+    });
+
+}
