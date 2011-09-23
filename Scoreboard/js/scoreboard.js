d4369cfbe8836dc8927828ab40a1c0777b1f68e4
Kyle Beikirch <kyle.beikirch@gmail.com>
2011-09-22 23:03:58 -0700

    Add divs to layout setup

--- Scoreboard/js/scoreboard.js
+++ Scoreboard/js/scoreboard.js
@@ -13,6 +13,7 @@ function xmlParser(xml) {
     $(xml).find("game").each(function() {
 		
 		console.log($(this).find("stadium"));
+		$("#scoreHolder").append('<div class="scoreCard"></div>');
         //$(".main").append('<div class="book"><div class="title">' + $(this).find("Title").text() + '</div><div class="description">' + $(this).find("Description").text() + '</div><div class="date">Published ' + $(this).find("Date").text() + '</div></div>');
         //$(".book").fadeIn(1000);
 
